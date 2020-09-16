0a4e3fb232923c87a2a8b5a6737b65351ed62d30
Alan Parley <alanparley@hotmail.com>
2020-09-16 20:41:33 +0100

    first 2 tests running aok

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,2 +1,6 @@
-def compare():
-    pass
+def compare(num_1, num_2):
+    return f"{num_1} is greater than {num_2}"
+
+
+def compare_2(num_3, num_4):
+    return f"{num_3} is less than {num_4}"
--- start_code/tests/compare_test.py
+++ start_code/tests/compare_test.py
@@ -1,8 +1,13 @@
 import unittest
 
 from src.compare import compare
+from src.compare import compare_2
+
 
 class TestCompare(unittest.TestCase):
 
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_1_3_returns_1_is_less_than_3(self):
+        self.assertEqual("1 is less than 3", compare_2(1, 3))
